8878d56a9e275b942056315d60e47a22674fc796
jkrowden <jkrowden@gmail.com>
2018-04-19 19:58:16 -0600

    basic server layout complete

--- restaurant.js
+++ restaurant.js
@@ -6,6 +6,9 @@ var mysql = require("mysql");
 var app = express();
 var PORT = 3000;
 
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+
 var connection = mysql.createConnection({
 	host: 'localhost',
 	port: 3306,
@@ -14,4 +17,39 @@ var connection = mysql.createConnection({
 	password: 'goober',
 
 	database: 'restaurant'
+});
+
+app.get("/", function (req, res) {
+	res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/createreservation", function (req, res) {
+	res.sendFile(path.join(__dirname, "reservations.html"));
+});
+
+app.get("/table", function (req, res) {
+	res.sendFile(path.join(__dirname, "table.html"));
+});
+
+app.get("/api/:table", function (req, res) {
+	var table = req.params.table;
+
+	if (table === 'reservations') {
+		//query mysql reservations table
+	} else if (table === 'waitlist') {
+		//query mysql waitlist table
+	} else {
+		//display that no api was found
+	}
+});
+
+app.post("/api/reserve", function (req, res) {
+	//get values from reservations page
+	//query sql INSERT INTO
+	//return whether it was inserted or not
+});
+
+
+app.listen(PORT, function () {
+	console.log("App listening on port " + PORT);
 });
